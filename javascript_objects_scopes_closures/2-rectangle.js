aa10257528a765e0831aae15efc4c1d65bc2cc90
Semira Hussien <semutiye22@gmail.com>
2024-02-01 10:37:20 +0300

    hello

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,15 @@
+// Filename: 2-rectangle.js
+
+class Rectangle {
+    constructor(w, h) {
+      if (w <= 0 || h <= 0) {
+        // Create an empty object if w or h is not a positive integer
+        return {};
+      }
+  
+      this.width = w;
+      this.height = h;
+    }
+  }
+  
+  module.exports = Rectangle;
